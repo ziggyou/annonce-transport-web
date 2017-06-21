92ef94cc99ae4c5893945aa995c18698d8e7cc4f
zito <antoinemendy95@gmail.com>
2017-06-21 19:59:18 +0000

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,7 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-	username : string = '';
+    username : string = '';
   	password: string;
 
   constructor() {
