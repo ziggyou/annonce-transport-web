1afc778bb8c3a328a8f1f6095fb47c5a1bc5e298
mbaye diop <mbaye.diop@suite.sn>
2017-07-04 02:22:39 +0000

    addition of many things, I no more knows how many

--- src/app/liste-annonce/liste-annonce.component.ts
+++ src/app/liste-annonce/liste-annonce.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {ListeAnnonceService} from "./liste-annonce.service";
 
 @Component({
   selector: 'app-liste-annonce',
   templateUrl: './liste-annonce.component.html',
-  styleUrls: ['./liste-annonce.component.css']
+  styleUrls: ['./liste-annonce.component.css'],
+  providers : [ListeAnnonceService]
 })
 export class ListeAnnonceComponent implements OnInit {
 
-  constructor() { }
+  annonces;
+  constructor(private listeAnnonceService : ListeAnnonceService) { }
 
   ngOnInit() {
+    this.listeAnnonceService.getAnnonces(localStorage.getItem("userId"))
+      .subscribe(
+        data => this.handleAnnonces(data)
+      );
+  }
+
+  handleAnnonces(data){
+    console.log(data)
+    this.annonces = data;
+  }
+
+  annoncedetails(annonce)
+  {
+    localStorage.setItem("annonceId", annonce.id);
   }
 
 }
+
--- src/app/liste-annonce/liste-annonce.service.ts
+++ src/app/liste-annonce/liste-annonce.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import {Http, Response} from "@angular/http";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class ListeAnnonceService {
 
-  constructor() { }
+  base_Url = "https://annonces-transport-api.herokuapp.com/api";
+  constructor(private http: Http) {
+  }
 
+  getAnnonces(id) {
+    return this.http.get(this.base_Url + "/Utilisateurs/" + id + "/annonces/")
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
 }
